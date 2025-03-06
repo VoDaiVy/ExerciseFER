d565951030e5558dc05663b1140173766d7ab0c3
VoDaiVy <vyvdde180817@fpt.edu.vn>
2025-03-06 14:34:44 +0700

    ex9-1

---
+++ ex9-1/src/AboutMe.js
@@ -0,0 +1,12 @@
+
+
+function AboutMe() {
+  return (
+      <div>
+          <h1>Vo Dai Vy</h1>
+          <p>I am an adventurous person, exploring new and unique places in the world.</p>
+      </div>
+  );
+}
+  export default AboutMe;
+  
---
+++ ex9-1/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import AboutMe from "./AboutMe";
+
+function App() {
+  return (
+    <div className="App">
+      <AboutMe />
+    </div>
+  );
+}
+
+export default App;
+
+
